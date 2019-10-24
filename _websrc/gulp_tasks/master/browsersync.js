6e8888bba61f7a85f6f0014a7e7c3be73756e06d
iwiedenm <iwiedenm@lion.lmu.edu>
2019-10-24 03:48:13 -0700

    Perf marker, ngrok, .gitignore remove GEN files

--- _websrc/gulp_tasks/master/browsersync.js
+++ _websrc/gulp_tasks/master/browsersync.js
@@ -2,6 +2,7 @@ const argv        = require('yargs').argv;
 const browsersync = require('browser-sync').create();
 const config      = require('../../master.config.js');
 const cp          = require('child_process');
+const cmd         = require('node-cmd');
 const gulp        = require('gulp');
 
 let browser = (config.browsersync.browsers[0] != null) ? config.browsersync.browsers : 'default';
@@ -10,13 +11,31 @@ let browser = (config.browsersync.browsers[0] != null) ? config.browsersync.brow
  * Wait for jekyll-build, then launch the Server
  */
 gulp.task('browsersync', ['jekyll-build'], function () {
-  return browsersync.init({
+  browsersync.init({
     port: config.port,
     browser: browser,
     server: {
       baseDir: config.jekyll.dest,
     }
+  }, function (error, instance) {
+    // Launch ngrok if enabled
+    if (!error && argv.ngrokOpen == 'true') {
+      const ngrok = require('/usr/local/lib/node_modules/ngrok');
+      (async function() {
+        const url = await ngrok.connect(instance.options.get('port'));
+        console.log('\x1b[40m\x1b[35m');
+        console.log('');
+        console.log('');
+        console.log('******* ngrok url *******');
+        console.log(url);
+        console.log('*************************');
+        console.log('');
+        console.log('\x1b[0m');
+        // cmd.run(`mkdir -p @output/ngrok/ && echo '<meta http-equiv="Refresh" content="0; url=${url}" />' >@output/ngrok/index.html`);
+      })();
+    }
   });
+
 });
 
 /**
