963d8c9128de3beb732b4619d41b206a5a0e306f
iwiedenm <iwiedenm@lion.lmu.edu>
2019-10-25 02:17:43 -0700

    Update update.js

--- _websrc/gulp_tasks/master/update.js
+++ _websrc/gulp_tasks/master/update.js
@@ -1,4 +1,4 @@
-const gulp     = require('gulp');
+const gulp = require('gulp');
 
 gulp.task('update', async function () {
   cmd.run(`git checkout template && git fetch upstream && git pull upstream && git checkout master && git merge template -m "Merged from template." && git push origin`);
