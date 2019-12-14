3af16be9f9d818408c703cde27c2c1db9fcbbeb1
iwiedenm <iwiedenm@lion.lmu.edu>
2019-12-13 19:22:16 -0800

    Update test

---
+++ _websrc/gulp_tasks/master/_commands.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp');
+
+gulp.task('cloudflare:purge', async function (done) {
+  console.log('RAN', 'cloudflare:purge');
+  done();
+});
--- _websrc/gulp_tasks/master/build.js
+++ _websrc/gulp_tasks/master/build.js
@@ -3,7 +3,7 @@ let config = require('../../master.config.js');
 let appGulpTasks = require('../../app.config.js');
 const cmd      = require('node-cmd');
 let yaml = require('js-yaml');
-let fs = require('fs');
+let fs = require('fs-jetpack');
 // let webManager = require('web-manager/package.json').version;
 
 config.tasks = Object.assign(config.tasks, appGulpTasks.tasks);
@@ -22,56 +22,69 @@ console.log('Command line args => ', argv);
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
-  let jekyllConfig = config.jekyll.config.default;
-  jekyllConfig += config.jekyll.config.app ? ',' + config.jekyll.config.app : '';
+gulp.task('jekyll-build', async function (done) {
+  await new Promise(async function(resolve, reject) {
+    let jekyllConfig = config.jekyll.config.default;
+    jekyllConfig += config.jekyll.config.app ? ',' + config.jekyll.config.app : '';
 
-  if (argv.jekyllEnv == 'production') {
-    process.env.JEKYLL_ENV = 'production';
-    jekyllConfig += config.jekyll.config.production ? ',' + config.jekyll.config.production : '';
-  } else {
-    jekyllConfig += config.jekyll.config.development ? ',' + config.jekyll.config.development : '';
-    jekyllConfig += ',' + '@output/.temp/_config_browsersync.yml';
-  }
+    if (argv.jekyllEnv == 'production') {
+      process.env.JEKYLL_ENV = 'production';
+      jekyllConfig += config.jekyll.config.production ? ',' + config.jekyll.config.production : '';
+    } else {
+      jekyllConfig += config.jekyll.config.development ? ',' + config.jekyll.config.development : '';
+      jekyllConfig += ',' + '@output/.temp/_config_browsersync.yml';
+    }
 
-  // Create build log JSON
-  cmd.run('' +
-  'build_log_path="@output/build/build.json"' + ' && ' +
-  'sed "s/%TIMESTAMP_UTC_NPM%/' + now({offset: 0}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' + ' && ' +
-  'sed "s/%TIMESTAMP_PST_NPM%/' + now({offset: -7}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' + ' && ' +
-  'sed "s/%VERSION_WEB_MANAGER%/' + require('web-manager/package.json').version + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' +
-  '');
+    // Create build log JSON
+    try {
+      let build = JSON.parse(fs.read('@output/build/build.json'));
+      build['npm-build'].timestamp_utc = now({offset: 0});
+      build['npm-build'].timestamp_pst = now({offset: -7});
+      build.packages['web-manager'] = require('web-manager/package.json').version;
+      fs.write('@output/build/build.json', JSON.stringify(build, null, 2));
+    } catch (e) {
+      console.error('Error updating build.json', e);
+    }
 
-  if (argv.buildLocation == 'server') {
     // Create CloudFlare Zone File
-    let doc = yaml.safeLoad(fs.readFileSync('_config.yml', 'utf8'));
-    cmd.run(`rm -rf @output/.temp && mkdir -p @output/.temp && echo '${doc.cloudflare.zone}' >@output/.temp/cloudflare-zone.txt`);
+    try {
+      let doc = yaml.safeLoad(fs.read('_config.yml'));
+      fs.write('@output/.temp/cloudflare-zone.txt', doc.cloudflare.zone);
+    } catch (e) {
+      console.error('Error creating cloudflare-zone.txt', e);
+    }
 
-    // // Create build log JSON
-    // cmd.run('' +
-    // 'build_log_path="@output/templated/build.json"' + ' && ' +
-    // 'sed "s/%TIMESTAMP_UTC_NPM%/' + now({offset: 0}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' + ' && ' +
-    // 'sed "s/%TIMESTAMP_PST_NPM%/' + now({offset: -7}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' +
-    // '');
-    // cmd.run('' +
-    // 'build_log_path="_websrc/templates/master/build/build.json"' + ' && ' +
-    // 'build_log_path_dest="@output/build/build.json"' + ' && ' +
-    // 'sed "s/%TIMESTAMP_UTC_NPM%/' + now({offset: 0}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' + ' && ' +
-    // 'sed "s/%TIMESTAMP_PST_NPM%/' + now({offset: -7}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' +
-    // '');
-  } else {
-    // console.log('buildLocation =', 'local');
-  }
+    if (argv.buildLocation == 'server') {
+      // Create CloudFlare Zone File
+      // let doc = yaml.safeLoad(fs.read('_config.yml'));
+      // cmd.run(`rm -rf @output/.temp && mkdir -p @output/.temp && echo '${doc.cloudflare.zone}' >@output/.temp/cloudflare-zone.txt`);
 
-  if (argv.skipJekyll == 'true') {
-    // console.log('skipJekyll =', true);
-    return done();
-  } else {
-    // console.log('skipJekyll =', false);
-    return cp.spawn(jekyll, ['build', '--config', jekyllConfig], {stdio: 'inherit', env: process.env})
-      .on('close', done);
-    // update 1
-  }
+      // // Create build log JSON
+      // cmd.run('' +
+      // 'build_log_path="@output/templated/build.json"' + ' && ' +
+      // 'sed "s/%TIMESTAMP_UTC_NPM%/' + now({offset: 0}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' + ' && ' +
+      // 'sed "s/%TIMESTAMP_PST_NPM%/' + now({offset: -7}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' +
+      // '');
+      // cmd.run('' +
+      // 'build_log_path="_websrc/templates/master/build/build.json"' + ' && ' +
+      // 'build_log_path_dest="@output/build/build.json"' + ' && ' +
+      // 'sed "s/%TIMESTAMP_UTC_NPM%/' + now({offset: 0}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' + ' && ' +
+      // 'sed "s/%TIMESTAMP_PST_NPM%/' + now({offset: -7}) + '/g" $build_log_path > "$build_log_path"-temp && mv "$build_log_path"-temp $build_log_path' +
+      // '');
+    } else {
+      // console.log('buildLocation =', 'local');
+    }
+
+    if (argv.skipJekyll == 'true') {
+      // console.log('skipJekyll =', true);
+      return done();
+    } else {
+      // console.log('skipJekyll =', false);
+      return cp.spawn(jekyll, ['build', '--config', jekyllConfig], {stdio: 'inherit', env: process.env})
+        .on('close', done);
+      // update 1
+    }
+  });
 });
 
 /**
