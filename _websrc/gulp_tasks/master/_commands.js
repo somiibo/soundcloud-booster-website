4be1247e43f71f2364e8791c08873073ad75953d
iwiedenm <iwiedenm@lion.lmu.edu>
2019-12-13 19:27:40 -0800

    Update _commands.js

--- _websrc/gulp_tasks/master/_commands.js
+++ _websrc/gulp_tasks/master/_commands.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 
 gulp.task('cloudflare:purge', async function (done) {
-  console.log('RAN', 'cloudflare:purge');
-  done();
+  await new Promise(function(resolve, reject) {
+    console.log('RAN', 'cloudflare:purge');
+    resolve();
+  });
 });
