94cda6e97b24e4ce1380557d51adda50afb2b967
iwiedenm <iwiedenm@lion.lmu.edu>
2020-04-27 18:30:59 -0700

    Update main.js

--- assets/_src/js/master/main.js
+++ assets/_src/js/master/main.js
@@ -1,5 +1,5 @@
 Manager = new (require('web-manager'));
-Manager = new (require('/Users/ianwiedenman/Documents/GitHub/ITW-Creative-Works/web-manager'));
+// Manager = new (require('/Users/ianwiedenman/Documents/GitHub/ITW-Creative-Works/web-manager'));
 
 Manager.init(Configuration, function() {
   Manager.log('Init main.js');
