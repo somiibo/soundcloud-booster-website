fd96d6bd56fad9df9a11a66b818a4a9b2955cbb5
iwiedenm <ian.wiedenman@gmail.com>
2020-07-21 02:48:11 -0700

    Fix eslint

--- _websrc/gulp_tasks/master/eslint.js
+++ _websrc/gulp_tasks/master/eslint.js
@@ -1,11 +1,14 @@
-const config = require('../../master.config.js');
+const argv      = require('yargs').argv;
+const config    = require('../../master.config.js');
 const configApp = require('../../app.config.js');
-const eslint = require('gulp-eslint');
-const gulp   = require('gulp');
+const eslint    = require('gulp-eslint');
+const gulp      = require('gulp');
 
 gulp.task('eslint', function() {
-  return gulp.src([config.assets + config.assetsSubpath + '/' + config.js.src + '/**/*.js', '!node_modules/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+  if (argv.skipESLint !== 'true') {
+    return gulp.src([config.assets + config.assetsSubpath + '/' + config.js.src + '/**/*.js', '!node_modules/**'])
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failOnError());
+  }
 });
--- _websrc/gulp_tasks/master/webpack.js
+++ _websrc/gulp_tasks/master/webpack.js
@@ -11,12 +11,7 @@ const path           = require('path');
 let tools            = new (require('../../libraries/tools.js'));
 let Global           = require('../../libraries/global.js');
 
-// const entry = [];
-// for (var i = 0; i <= config.js.entry.length - 1; i++) {
-//   entry.push(config.assets + config.assetsSubpath + '/' + config.js.src + '/' + config.js.entry[i]);
-// }
-
-if (config.tasks.eslint && argv.skipESLint != 'true') {
+if (config.tasks.eslint && argv.skipESLint !== 'true') {
   config_webpack.module.rules.push(config.eslintLoader);
 }
 
@@ -28,51 +23,12 @@ gulp.task('webpack', async function () {
   await tools.poll(function () {
     return Global.get('prefillStatus') === 'done';
   }, {timeout: 60000});
-  // await gulp.src([`${prePath}/**/*.js`, `!${prePath}/${config.js.entry[0]}`, `!${prePath}/app/service-worker.js`] )
-  //   .pipe(plumber())
-  //   .pipe(named())
-  //   .pipe(babel(config.babel))
-  //   .pipe(webpackStream(config_webpack, webpack))
-  //   .pipe(gulp.dest(`${config.assets}/${config.js.dest}`));
-
-  // await gulp.src([`${prePath}/${config.js.entry[0]}`] )
-  //   .pipe(plumber())
-  //   .pipe(named())
-  //   .pipe(babel(config.babel))
-  //   .pipe(webpackStream(config_webpack, webpack))
-  //   .pipe(gulp.dest(`${config.assets}/${config.js.dest}`));
-
   await gulp.src([`${prePath}/**/*.js`, `!${prePath}/app/app.js`, `!${prePath}/app/service-worker.js`])
     .pipe(plumber())
     .pipe(named())
     .pipe(babel(config.babel))
     .pipe(webpackStream(config_webpack, webpack))
     .pipe(gulp.dest(`${config.assets}/${config.js.dest}`));
-
-  // await gulp.src([`${prePath}/**/*.js`, `!${prePath}/app/app.js`, `!${prePath}/app/service-worker.js`])
-  //   .pipe(plumber())
-  //   .pipe(named())
-  //   .pipe(babel(config.babel))
-  //   .pipe(webpackStream(config_webpack, webpack))
-  //   .pipe(gulp.dest(function (file) {
-  //     const dir = file.dirname;
-  //     let final = '';
-  //     // if (file.basename.includes()) {
-  //     //
-  //     // }
-  //     // console.log('=====> dirname', file.dirname);
-  //     if (file.basename === 'test.js') {
-  //       console.log('=====>');
-  //       console.log('base', file.base);
-  //       console.log('basename', file.basename);
-  //       console.log('cwd', file.cwd);
-  //       console.log('path', file.path);
-  //       console.log('relative', file.relative);
-  //       console.log('dirname', file.dirname);
-  //       console.log('stem', file.stem);
-  //     }
-  //     return path.join(file.base, config.assets, config.js.dest);
-  //   }));
 
 });
 
