e1b84b89922ec6b254a61ae5281d38bc6a50e0f3
Ian <ian.wiedenman@gmail.com>
2022-04-07 17:48:51 -0700

    Update create post

--- _websrc/gulp_tasks/master/create-post.js
+++ _websrc/gulp_tasks/master/create-post.js
@@ -50,6 +50,7 @@ Post.prototype.create = async function (options) {
           resolve();
         });
       }
+      // console.log('----body 1', body);
       let finalPost = await poster.create(JSON.parse(body))
       .catch(function (e) {
         response.status = 500;
@@ -72,6 +73,7 @@ Post.prototype.create = async function (options) {
 
       if (response.status == 200) {
         poster.write(finalPost.path, finalPost.content);
+        response.data = finalPost;
       }
 
       res.statusCode = response.status;
