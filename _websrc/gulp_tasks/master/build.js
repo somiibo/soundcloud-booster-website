7e606dd8a6b615acafbe4cec7af35b4ebf77f686
Ian <ian.wiedenman@gmail.com>
2023-11-15 13:23:53 -0800

    Update build.js

--- _websrc/gulp_tasks/master/build.js
+++ _websrc/gulp_tasks/master/build.js
@@ -1,12 +1,12 @@
 const argv         = require('yargs').argv;
 const config       = require('../../master.config.js');
 const appGulpTasks = require('../../app.config.js');
-const fs           = require('fs-jetpack');
+const jetpack      = require('fs-jetpack');
 const cp           = require('child_process');
 const gulp         = require('gulp');
 const tools        = new (require('../../libraries/tools.js'));
 const JSON5        = require('json5');
-const _configYml   = require('js-yaml').load(fs.read('_config.yml'));
+const _configYml   = require('js-yaml').load(jetpack.read('_config.yml'));
 const Global       = require('../../libraries/global.js');
 
 config.tasks = Object.assign(config.tasks, appGulpTasks.tasks);
@@ -49,7 +49,7 @@ gulp.task('jekyll-build', async () => {
   await tools.poll(() => areTasksCompleted(), { timeout: 1000 * 60 * 3 });
 
   await tools.poll(() => {
-    return fs.exists('./assets/js/main.js') && fs.exists('./assets/css/main.css');
+    return jetpack.exists('./assets/js/main.js') && jetpack.exists('./assets/css/main.css');
   }, {timeout: 1000 * 60 * 3});
 
   // console.log('jekyll-build: precheck complete', new Date());
@@ -75,7 +75,7 @@ gulp.task('jekyll-build', async () => {
 
   // Create build log JSON
   try {
-    let buildJSON = JSON.parse(fs.read('@output/build/build.json'));
+    let buildJSON = JSON.parse(jetpack.read('@output/build/build.json'));
     buildJSON['npm-build'].timestamp = new Date().toISOString();
 
     let info = await getGitInfo();
@@ -93,7 +93,7 @@ gulp.task('jekyll-build', async () => {
     // Set custom admin dashboard pages
     buildJSON['admin-dashboard'] = JSON5.parse(_configYml['admin-dashboard']);
 
-    fs.write('@output/build/build.json', JSON.stringify(buildJSON, null, 2));
+    jetpack.write('@output/build/build.json', JSON.stringify(buildJSON, null, 2));
   } catch (e) {
     console.error('Error updating build.json', e);
   }
@@ -153,7 +153,7 @@ async function getGitInfo() {
 function postBuild() {
   return new Promise(function(resolve, reject) {
     // Move _site/blog/index.html to blog.html
-    fs.move('_site/blog/index.html', '_site/blog.html');
+    jetpack.move('_site/blog/index.html', '_site/blog.html');
 
     return resolve();
   });
